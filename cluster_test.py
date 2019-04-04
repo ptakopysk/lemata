dfaa1f4d8816778d168543b5608b64df20120307
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-04-04 19:36:09 +0200

    unsuccessful expriments with sparsification

--- cluster_test.py
+++ cluster_test.py
@@ -11,6 +11,8 @@ from sklearn.cluster import AgglomerativeClustering
 
 from sklearn.metrics.pairwise import pairwise_distances
 
+from scipy.sparse import dok_matrix
+
 from pyjarowinkler import distance
 
 def jwdist(word, otherword):
@@ -38,20 +40,44 @@ index2word = [
 
 N = len(index2word)
 
+def get_stem(form):
+    #return form[:2]
+    return form[:1]
+    #return 'a'
+
 word2index = dict()
 index = 0
 for index, word in enumerate(index2word):
     word2index[word] = index
 
-
-D = np.zeros((N,N))
+# D = np.zeros((N,N))
+# sparse matrix
+D = dok_matrix((N,N), dtype=float)
+# TODO only iterate over what makes sense
 for i1 in range(N):
     for i2 in range(N):
-        D[i1,i2] = jwdist(index2word[i1], index2word[i2])
+        if get_stem(index2word[i1]) == get_stem(index2word[i2]):
+            D[i1,i2] = jwdist(index2word[i1], index2word[i2])
 
-clustering = AgglomerativeClustering(affinity='precomputed', linkage='average')
+#print(D)
 
-labels = clustering.fit_predict(D)
+DD = np.zeros((N,N))
+for i1 in range(N):
+    for i2 in range(N):
+        DD[i1,i2] = jwdist(index2word[i1], index2word[i2])
+
+print(DD)
+
+L = 'average'
+#L = 'single'
+#L = 'complete'
+clustering = AgglomerativeClustering(affinity='precomputed',
+        linkage=L,
+        connectivity = D,
+        compute_full_tree = False,
+        n_clusters=3)
+
+labels = clustering.fit_predict(DD)
 # print(labels)
 
 label2words = defaultdict(list)
