872d4a9ab73dd473f595233e686af70197d78771
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-04-09 18:47:43 +0200

    inbdicesright

--- cluster.py
+++ cluster.py
@@ -273,7 +273,7 @@ for stem in iterate_over:
 
     if args.plot:
         # at the i-th iteration, children[i][0] and children[i][1] are merged to form node n_samples + i
-        index = 0
+        index = I
         for merge in clustering.children_:
             print(index, merge[0], merge[1])
             index += 1
