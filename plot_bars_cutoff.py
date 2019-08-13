bead9bd5f20fa89cd71bb60086f62bcbce8884fc
Rudolf Rosa <rosa@ufal.mff.cuni.cz>
2019-08-13 17:33:21 +0200

    visualisation

---
+++ plot_bars_cutoff.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+#coding: utf-8
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import colors
+from collections import defaultdict
+
+# dist
+ind_value = list()
+# infl/der
+ind_is_infl = list()
+
+with open(sys.argv[1]) as infile:
+    thresh_line = infile.readline().split()
+    thresh = float(thresh_line[2])
+    
+    # strip headers
+    infile.readline()
+    for line in infile:
+        line = line.rstrip('\n')
+        items = line.split("\t")
+        if (items[0].startswith('===')):
+            ind_value.append(1.0)
+            ind_is_infl.append('r')
+        else:
+            dist = float(items[3])
+            is_infl = 'b' if items[2] == 'INFL' else 'y'
+            ind_value.append(dist)
+            ind_is_infl.append(is_infl)
+        if dist > 2*thresh:
+            break
+
+plt.figure(1)
+
+#s = np.argsort(ind_value)
+#x = np.arange(len(ind_value))
+#plt.bar(x, height=[ind_value[k] for k in s], color=[ind_is_infl[k] for k in s])
+
+r = range(len(ind_value))
+plt.bar(r, height=ind_value, color=ind_is_infl)
+
+#plt.show()
+plt.savefig(sys.argv[1] + '.pdf')
+
--- run_clust_50l_top50roots_best_output_test.sh
+++ run_clust_50l_top50roots_best_output_test.sh
@@ -3,8 +3,8 @@ do
     # simpl
     for s in jwxcos
     do
-        qsub -N clust-pairs-simpl-$s-$(echo $r|unidecode) analyze_lc_50l_ROOT_SIM_PARAMS.shc $r $s -S -O -E pairs
-        qsub -N anal-thresh-simpl-$s-$(echo $r|unidecode) analyze_lc_50l_ROOT_SIM_PARAMS.shc $r $s -S -O -T
+        qsub -N clust-pairs-simpl-2-$s-$(echo $r|unidecode) analyze_lc_50l_ROOT_SIM_PARAMS.shc $r $s -S -O -E pairs
+        #qsub -N anal-thresh-simpl-$s-$(echo $r|unidecode) analyze_lc_50l_ROOT_SIM_PARAMS.shc $r $s -S -O -T
         sleep 0.1s
     done
 done
